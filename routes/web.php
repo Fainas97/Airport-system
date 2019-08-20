753bfda73da29104ff17bc29af0989f3b9d587c0
Fainas97 <karolis.fain@gmail.com>
2019-08-20 17:07:14 +0300

    Routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'airports'], function () {
+
+    Route::get('/', 'AirportController@index');
+    
+    Route::get('/create', 'AirportController@create')->name('air.create');
+    Route::post('/create', 'AirportController@store');
+
+    Route::get('/{id}', 'AirportController@show')->name('air.show');
+
+    Route::get('/{id}/edit', 'AirportController@edit')->name('air.edit');
+    Route::put('/{id}/update', 'AirportController@update')->name('air.update');
+
+    Route::delete('/{id}', 'AirportController@destroy')->name('air.destroy');
 });
+
+Route::group(['prefix' => 'companies'], function () {
+
+    Route::get('/', 'CompanyController@index');
+    
+    Route::get('/create', 'CompanyController@create')->name('com.create');
+    Route::post('/create', 'CompanyController@store');
+
+    Route::get('/{id}', 'CompanyController@show')->name('com.show');
+
+    Route::get('/{id}/edit', 'CompanyController@edit')->name('com.edit');
+    Route::put('/{id}/update', 'CompanyController@update')->name('com.update');
+
+    Route::delete('/{id}', 'CompanyController@destroy')->name('com.destroy');
+});
+
+Route::group(['prefix' => 'countries'], function () {
+
+    Route::get('/', 'CountryController@index');
+});
+
+Route::any('/{any}', function () {
+    return redirect('/airports');
+})->where('any', '.*');
